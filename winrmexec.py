12a587953eb1254e01d686e03ddb5fbb45353e23
ozelis <2608015+ozelis@users.noreply.github.com>
2025-10-20 11:58:51 +0300

    Define new constants for session capabilities
    before i used some bad practice to add some globals dynamically that messes up with linters

--- winrmexec.py
+++ winrmexec.py
@@ -1,4 +1,4 @@
-import os, sys, re, uuid, logging, ssl, shlex
+import os, sys, re, uuid, logging, ssl
 
 from base64 import b64encode, b64decode
 from struct import pack, unpack
@@ -349,8 +349,38 @@ msg_ids = {
     0x00041101 : "PIPELINE_HOST_RESPONSE"
 }
 
-for k, v in msg_ids.items():
-    globals()[v] = k
+SESSION_CAPABILITY         =  0x00010002
+INIT_RUNSPACEPOOL          =  0x00010004
+PUBLIC_KEY                 =  0x00010005
+ENCRYPTED_SESSION_KEY      =  0x00010006
+PUBLIC_KEY_REQUEST         =  0x00010007
+CONNECT_RUNSPACEPOOL       =  0x00010008
+RUNSPACEPOOL_INIT_DATA     =  0x0002100B
+RESET_RUNSPACE_STATE       =  0x0002100C
+SET_MAX_RUNSPACES          =  0x00021002
+SET_MIN_RUNSPACES          =  0x00021003
+RUNSPACE_AVAILABILITY      =  0x00021004
+RUNSPACEPOOL_STATE         =  0x00021005
+CREATE_PIPELINE            =  0x00021006
+GET_AVAILABLE_RUNSPACES    =  0x00021007
+USER_EVENT                 =  0x00021008
+APPLICATION_PRIVATE_DATA   =  0x00021009
+GET_COMMAND_METADATA       =  0x0002100A
+RUNSPACEPOOL_HOST_CALL     =  0x00021100
+RUNSPACEPOOL_HOST_RESPONSE =  0x00021101
+PIPELINE_INPUT             =  0x00041002
+END_OF_PIPELINE_INPUT      =  0x00041003
+PIPELINE_OUTPUT            =  0x00041004
+ERROR_RECORD               =  0x00041005
+PIPELINE_STATE             =  0x00041006
+DEBUG_RECORD               =  0x00041007
+VERBOSE_RECORD             =  0x00041008
+WARNING_RECORD             =  0x00041009
+PROGRESS_RECORD            =  0x00041010
+INFORMATION_RECORD         =  0x00041011
+PIPELINE_HOST_CALL         =  0x00041100
+PIPELINE_HOST_RESPONSE     =  0x00041101
+
 
 # -- transports: ----------------------------------------------------------------------------------
 class TransportError(Exception):
